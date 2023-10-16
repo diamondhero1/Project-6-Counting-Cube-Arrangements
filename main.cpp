2e96f7b5cecabf0ec12ce3b998d207b46dc06fbd
clee24 <clee24@wooster.edu>
2023-10-16 11:59:35 -0400

    done

--- main.cpp
+++ main.cpp
@@ -2,4 +2,12 @@
 // Created by fowle on 9/29/2023.
 //
 
+
+class main {
+
+    printf("yoooo what's good");
+
+};
+
+
 #include "main.h"
--- main.h
+++ main.h
@@ -8,6 +8,8 @@
 
 class main {
 
+    printf("hello");
+
 };
 
 
