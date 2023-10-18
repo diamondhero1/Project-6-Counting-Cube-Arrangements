8924df59706dba0cf413a4ce321c5e8e9b91821b
clee24 <clee24@wooster.edu>
2023-10-18 12:43:25 -0400

    huzz

--- cubes.cpp
+++ cubes.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "cubes.h"
+using namespace std
+
+
+cube::cube(){
+    direction = 0;
+}
+
+void cube::getDirection(){
+
+}
+
+virtual void cube::rotate90(){
+}
+
+tunnel::tunnel(int dir){
+}
+
+void tunnel::rotate90(){
+
+}
+
+room::room(int dir){}
+
+void room::rotate90(){
+
+}
