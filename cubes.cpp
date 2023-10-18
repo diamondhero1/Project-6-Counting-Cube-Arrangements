17fea2af8fbd30db13aed4c5cd371184dda95727
clee24 <clee24@wooster.edu>
2023-10-18 12:41:09 -0400

    huzzah

---
+++ cubes.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Christopher Lee on 10/18/23.
+//
+
+#include "cubes.h"
---
+++ cubes.h
@@ -0,0 +1,16 @@
+//
+// Created by Christopher Lee on 10/18/23.
+//
+
+#ifndef PROJECT_6_COUNTING_CUBE_ARRANGEMENTS_CUBES_H
+#define PROJECT_6_COUNTING_CUBE_ARRANGEMENTS_CUBES_H
+
+
+
+class cubes {
+
+};
+
+
+
+#endif //PROJECT_6_COUNTING_CUBE_ARRANGEMENTS_CUBES_H
